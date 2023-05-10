01b7dded9e9b9f39f6a9089ef4770cf4c66a544f
Timo Sarkar <71646577+0xtimosarkar@users.noreply.github.com>
2023-05-10 10:32:56 +0200

    added hardhat deployment configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,34 @@
-require("@nomiclabs/hardhat-waffle");
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('dotenv').config(); // Required to load environment variables from .env file
+
+const { INFURA_PROJECT_ID, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  // Contract deployment scripts
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  // Configure the Solidity compiler
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
