7cdd10d3ecac0c00d40c4945453fd6508cda70fd
Timo Sarkar <71646577+0xtimosarkar@users.noreply.github.com>
2023-05-10 10:31:11 +0200

    updated deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,17 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const Memecoin = await hre.ethers.getContractFactory("memecoin");
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Memecoin = await ethers.getContractFactory("memecoin");
   const memecoin = await Memecoin.deploy();
 
-  await memecoin.deployed();
-
-  console.log("Memecoin deployed to:", memecoin.address);
+  console.log("memecoin address:", memecoin.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().then(() => process.exit(0)).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
